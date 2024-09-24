404de73c874116eed0977b3785ec1952ae29e37a
stelmon01 <stelmon01@gmail.com>
2024-09-24 20:15:43 +0545

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Make sure you're importing from 'react-dom/client'
 import App from "./App";
 
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
